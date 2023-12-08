6ecb1cd45ce5083a6feb7bb91b9fe56687540236
Orlando Hengelmolen <ohengelm@f1r5s15.codam.nl>
2023-12-08 16:47:51 +0100

    [EDIT] finished ex02 with allt ypes of iterators

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -26,13 +26,20 @@ class MutantStack: public std::stack<T>
 	public:
 		typedef T		*iterator;
 		typedef const T	*const_iterator;
+		//stl_list.h
+		typedef std::reverse_iterator<iterator> 		reverse_iterator;
+		typedef std::reverse_iterator<const_iterator>	const_reverse_iterator;
 
-		iterator	begin(void) {return &this->c.front();};
-		iterator	back(void) {return (&this->c.back());};	
-		iterator	end(void) {return (&this->c.back() + 1);};
-		const_iterator	begin(void) const {return &this->c.front();};
-		const_iterator	back(void) const {return (&this->c.back());};	
-		const_iterator	end(void) const {return (&this->c.back() + 1);};
+		iterator			begin(void)		{if (this->c.size() == 0) return (this->end()); return (&this->c.front());};
+		reverse_iterator	rbegin(void)	{if (this->c.size() == 0) return (this->rend()); return reverse_iterator(&this->c.back() + 1);};
+		iterator			back(void)		{return (&this->c.back());};	
+		iterator			end(void)		{return (&this->c.back() + 1);};
+		reverse_iterator	rend(void)		{return reverse_iterator(&this->c.front());};
+		const_iterator			begin(void) const	{if (this->c.size() == 0) return (this->end()); return (&this->c.front());};
+		const_reverse_iterator	rbegin(void) const	{if (this->c.size() == 0) return (this->rend()); return const_reverse_iterator(&this->c.back() + 1);};
+		const_iterator			back(void) const	{return (&this->c.back());};	
+		const_iterator			end(void) const		{return (&this->c.back() + 1);};
+		const_reverse_iterator	rend(void) const	{return const_reverse_iterator(&this->c.front());};
 
 		T	&operator[](unsigned int i) {return (this->c[i]);};
 		const T	&operator[](unsigned int i) const {return (this->c[i]);};
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,33 +6,39 @@
 /*   By: ohengelm <ohengelm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 20:29:42 by ohengelm          #+#    #+#             */
-/*   Updated: 2023/12/07 21:33:40 by ohengelm         ###   ########.fr       */
+/*   Updated: 2023/12/08 16:32:55 by ohengelm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include "print.hpp"
+#include "colors.hpp"
 
 #include <iostream>
 // std::cout
-// #include <vector>
+#include <cstdlib>
+// std::rand()
+#include <ctime>
+// std::time()
+#include <list>
+// std::list
 
-int	testSubject(void);
+int		testSubject(void);
+void	testManual(unsigned int size);
 
 int	main(void)
 {
+	print::headerLine("Exercise 02: Mutated abomination");
+	std::srand(std::time(NULL));
 	testSubject();
-	MutantStack<int>	mStack;
-
-	mStack.push(42);
-	std::cout	<< mStack[0]<<std::endl;
-	mStack[0] = 23;
-	std::cout	<< mStack[0]<<std::endl;
-	// std:vector<int> vector;
+	testManual(0);
+	testManual(4);
 	return (0);
 }
 
 int	testSubject(void)
 {
+	print::subHeaderLine("Subject Tests");
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -61,4 +67,59 @@ int	testSubject(void)
 	}
 	std::stack<int> s(mstack);
 	return 0;
+}
+
+void	testManual(unsigned int size)
+{
+	print::subHeaderLine("Manual test");
+	std::cout	<< C_DGRAY	<< "Creating containers MutantStack and std::list"
+				<< C_RESET	<< std::endl;
+	MutantStack<int>	mStack;
+	std::list<int>		list;
+
+	std::cout	<< C_DGRAY	<< "Randomly filling containers for size "	<< size
+				<< C_RESET	<< std::endl;
+	for (unsigned int i = 0; i < size; ++i)
+	{
+		int	random = std::rand() % 23;
+		mStack.push(random);
+		list.push_back(random);
+	}
+	std::cout	<< C_DGRAY	<< "Creating MutantStack copy"
+				<< C_RESET	<< std::endl;
+	const MutantStack<int>	mStackCopy(mStack);
+	std::cout	<< C_DGRAY	<< "\nPrinting contents using ::iterator"	<< '\n'
+				<< C_ORANGE	<< "MS / MS (copy) / list"
+				<< C_RESET	<< std::endl;
+	{
+		MutantStack<int>::iterator			im = mStack.begin();
+		MutantStack<int>::const_iterator	imc = mStackCopy.begin();
+		std::list<int>::iterator			il = list.begin();
+		while (im != mStack.end())
+		{
+			std::cout	<< *im	<< '/'	<< *imc	<< '/'	<< *il	<< '\n';
+			++im;
+			++imc;
+			++il;
+		}
+	}
+	std::cout	<< C_DGRAY	<< "\nIncrementing MutantStack's values by 1 using operator[]"
+				<< C_RESET	<< std::endl;
+	for (size_t i = 0; i < mStack.size(); ++i)
+		++mStack[i];
+	std::cout	<< C_DGRAY	<< "\nPrinting contents using ::const_reverse_iterator"	<< '\n'
+				<< C_ORANGE	<< "MS / MS (copy) / list"
+				<< C_RESET	<< std::endl;
+	{
+		MutantStack<int>::const_reverse_iterator	im = mStack.rbegin();
+		MutantStack<int>::const_reverse_iterator	imc = mStackCopy.rbegin();
+		std::list<int>::const_reverse_iterator		il = list.rbegin();
+		while (im != mStack.rend())
+		{
+			std::cout	<< *im	<< '/'	<< *imc	<< '/'	<< *il	<< '\n';
+			++im;
+			++imc;
+			++il;
+		}
+	}
 }
